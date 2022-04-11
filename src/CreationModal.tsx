4abdeb5f3a7af823705956155c39fe6b90b509e8
Dboss8 <divyanshm81@gmail.com>
2022-04-11 21:58:07 +0530

    Initial commit

---
+++ src/CreationModal.tsx
@@ -0,0 +1,94 @@
+import axios from "axios";
+import React from "react";
+import { Component } from "react";
+import {
+    Button,
+    Col,
+    Form,
+    Modal,
+    ModalBody,
+    ModalHeader,
+    Row,
+} from "reactstrap";
+
+
+type MyProps= {
+    isOpen : boolean
+    toggle 
+}
+
+export class CreationModal extends Component<MyProps>{
+
+    handleSubmit = (event) => {
+
+        event.preventDefault()
+        const data = new FormData(event.target)
+
+        const student = {
+            firstName : data.get("firstName"),
+            lastName : data.get("lastName"),
+            age : data.get("age"),
+            teacher : data.get("teacher"),
+        }
+
+        axios.post("http://localhost:7060/create", student)
+        this.props.toggle();
+    }
+
+    render(){
+
+        return (
+            <Modal isOpen= {this.props.isOpen} toggle= {this.props.toggle}>
+                <ModalHeader toggle= {this.props.toggle}>
+                    Adding new wise student
+                </ModalHeader>
+
+                <ModalBody>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Row>
+                            <Col>
+                                <label>Firstname</label>
+                            </Col>
+                            <Col>
+                                <input id="firstName" name="firstName" type="text"/>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <label>Lastname</label>
+                            </Col>
+                            <Col>
+                                <input id="lastName" name="lastName" type="text"/>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <label>Age</label>
+                            </Col>
+                            <Col>
+                                <input id="age" name="age" type="number"/>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col>
+                                <label>Teacher</label>
+                            </Col>
+                            <Col>
+                                <input id="teacher" name="teacher" type="text"/>
+                            </Col>
+                        </Row>
+
+                        <Button color="primary">
+                            Create New Student
+                        </Button>
+                    </Form>
+
+                </ModalBody>
+            </Modal>
+        )
+    
+    }
+}
---
+++ src/Dashboard.tsx
@@ -0,0 +1,194 @@
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Navbar,
+    NavbarBrand,
+    Container,
+    Row,
+    Col,
+    Card,
+    CardTitle,
+    CardText,
+    Button,
+    CardBody,
+    CardFooter
+} from 'reactstrap';
+import{
+    IoSchoolOutline,
+    IoMan,
+    IoSettings,
+  
+} from "react-icons/io5";
+import axios from "axios";
+import { CreationModal } from "./CreationModal";
+
+interface MyState{
+    isOpen : boolean;
+    students: [];
+}
+
+
+export class Dashboard extends Component<{}, MyState>{
+
+    state: MyState = {
+        isOpen : false,
+        students: [],
+    }
+
+    componentDidMount(){
+        axios.get("http://localhost:7060/list")
+        .then(res =>{
+            
+            const students = res.data
+            this.setState({students})
+        })
+        
+        
+    }
+
+    toggle = () => {
+        this.setState((prevState) => ({isOpen : !prevState.isOpen}))
+    }
+
+    render(){
+        
+        return(
+            <div>
+
+                <Navbar color= "dark" light mb-2>
+
+                    <NavbarBrand className="text-white">
+
+                        <IoSchoolOutline className="font-size-xxl"/>
+                        <span className="font-size-l ml-3">School Management Application</span>
+
+                    </NavbarBrand>
+
+                </Navbar>
+
+                <Container className="mt-3">
+
+                    <Row>
+                        <Col sm="4">
+                            <Card body>
+                                <CardTitle tag="h5">
+                                    <IoMan className="font-size-xl"/>86 students
+                                </CardTitle>
+
+                                <CardText>
+                                    with supporting text below is natural
+                                </CardText>
+
+                                <Button>Manage Students</Button>
+                            </Card>    
+                        </Col>
+
+                        <Col sm="4">
+                            <Card body>
+                                <CardTitle tag="h5">
+                                    <IoSchoolOutline className="font-size-xl"/>20 teacher
+                                </CardTitle>
+
+                                <CardText>
+                                    with supporting text below is natural
+                                </CardText>
+
+                                <Button>Manage Teachers</Button>
+                            </Card>   
+                        </Col>
+
+                        <Col sm="4">
+                            <Card body>
+                                <CardTitle tag="h5">
+                                    <IoSettings className="font-size-xl"/>13 employees
+                                </CardTitle>
+
+                                <CardText>
+                                    with supporting text below is natural
+                                </CardText>
+
+                                <Button>Manage Employees</Button>
+                            </Card>       
+                        </Col>
+                    </Row>
+
+                </Container>
+
+                <CreationModal isOpen= {this.state.isOpen} toggle= {this.toggle}></CreationModal>
+
+                <Container className="mt-4">
+                    <Row>
+                        <Col sm="12">
+                            <Button block color="success" onClick={this.toggle}>
+                                <span className="font-size-l">Add new student</span>
+                            </Button>    
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container className="mt-4">
+                    {this.state.students.map(student => renderStudent(student))}
+                </Container>
+
+            </div>
+        );
+    }
+
+
+
+}
+
+function renderStudent(st) {
+
+        return (
+            <Row>
+                <Col sm="12">
+                    <Card body>
+                        <CardTitle tag="h5">
+                            <IoMan className="font-size-xl"/>{st.firstName + " " + st.lastName}
+                        </CardTitle>
+    
+                        <CardBody>
+                            <Row>
+                                <Col sm="4" className="text-center">
+                                    <span className="font-weight-bold">Class</span>
+                                    <span>Second Year</span>
+                                </Col>
+    
+                                <Col sm="4" className="text-center">
+                                    <span className="font-weight-bold">Age</span>
+                                    <span>{st.age}</span>
+                                </Col>
+    
+                                <Col sm="4" className="text-center">
+                                    <span className="font-weight-bold">Teacher</span>
+                                    <span>{st.teacher}</span>
+                                </Col>
+                            </Row>
+                        </CardBody>
+    
+                        <CardFooter>
+                            <Row>
+                                <Col sm-6>
+                                    <Button block outline color="primary">
+                                        Edit
+                                    </Button>
+                                </Col>
+    
+                                <Col sm-6>
+                                    <Button block outline color="danger" onClick={() => deleteStudent(st.id)}>
+                                        Delete
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </CardFooter>
+                    </Card>    
+                </Col>    
+            </Row>
+        )
+    
+}
+function deleteStudent(id: any): void {
+    axios.post(`http://localhost:7060/delete/${id}`)
+}
+
